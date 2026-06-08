ea9b0190d1e2cafd01ae14f46f87ad1e323aba67
meeeehir123 <tripathimeehir@gmail.com>
2026-06-08 21:11:44 +0530

    College assignment all day 2 question

---
+++ DAY-02QUES2.cpp
@@ -0,0 +1,61 @@
+/*Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+
+Output: true
+
+Explanation:
+
+The element 1 occurs at the indices 0 and 3.
+
+Example 2:
+
+Input: nums = [1,2,3,4]
+
+Output: false
+
+Explanation:
+
+All elements are distinct.
+
+Example 3:
+
+Input: nums = [1,1,1,3,3,4,3,2,4,2]
+
+Output: true
+
+*/
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> seen;
+
+        for (int num : nums) {
+            if (seen.count(num)) {
+                return true;
+            }
+            seen.insert(num);
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1};
+
+    Solution sol;
+
+    cout << sol.containsDuplicate(nums);
+
+    return 0;
+}
